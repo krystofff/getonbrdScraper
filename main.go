0ac0830c858ab646e2a2223ab919e80e9082ed17
krystofff <ch.camposr@gmail.com>
2021-01-07 21:18:50 -0300

    preliminary approach to extract total values

--- main.go
+++ main.go
@@ -3,33 +3,25 @@ package main
 import (
 	"fmt"
 	"regexp"
+	"strconv"
+	"strings"
 
 	"github.com/gocolly/colly"
 )
 
-var jobIDs []string
+var jobIDs, jobTypeLinks, jobDetailsLinks []string
 
-var jobTypeLinks []string
-var jobDetailsLinks []string
+var uxJobs, programmingJobs, dsJobs, mobileJobs, supportJobs, marketingJobs, devopsQAJobs, opsManagementJobs,
+	salesJobs, advertisingJobs, agileJobs, hrJobs []jobDetails
 
-var uxJobs []jobDetails
-var programmingJobs []jobDetails
-var dsJobs []jobDetails
-var mobileJobs []jobDetails
-var supportJobs []jobDetails
-var marketingJobs []jobDetails
-var devopsQAJobs []jobDetails
-var opsManagementJobs []jobDetails
-var salesJobs []jobDetails
-var advertisingJobs []jobDetails
-var agileJobs []jobDetails
-var hrJobs []jobDetails
+var totalJobs, totalUxJobs, totalProgrammingJobs, totalDsJobs, totalMobileJobs, totalSupportJobs, totalMarketingJobs,
+	totalDevopsQAJobs, totalOpsManagementJobs, totalSalesJobs, totalAdvertisingJobs, totalAgileJobs, totalHrJobs int
 
 type jobDetails struct {
 	Type  string
 	Title string
 	// Role      string
-	Salary    string
+	Salary    int64
 	Location  string
 	Seniority string
 	Mode      string
@@ -60,12 +52,16 @@ func main() {
 	defaultCollector.Visit("https://www.getonbrd.com")
 
 	// start scrapping by job id/type
-	for i := range jobTypeLinks {
-		jobCollector.Visit(jobTypeLinks[i])
-	}
+	/* for i := range jobTypeLinks {
+	jobCollector.Visit(jobTypeLinks[i])
+	} */
+	jobCollector.Visit(jobTypeLinks[1])
 
 	//fmt.Println("UX jobs: ", uxJobs)
 	//fmt.Println("Programming  jobs: ", programmingJobs)
+	//fmt.Println("total UX josbs: ", totalUxJobs)
+	fmt.Println("total MObile josbs: ", totalMobileJobs)
+	fmt.Println("total jobs: ", totalJobs)
 }
 
 // default collector get job IDs
@@ -87,46 +83,98 @@ func getJobs() {
 		jobCollector.OnHTML("div[id=right-col]", func(e *colly.HTMLElement) {
 			location := e.ChildText("span[itemprop=address] > a[href] > span.location > span.tooltipster")
 			salary := e.ChildText("span[itemprop=baseSalary] > span.tooltipster-basic > strong")
+
+			fmt.Println("salary --> ", salary)
+
+			//TODO: refactor
 			// replace new lines
-			re := regexp.MustCompile(`\r?\n`)
+			regxNewline := regexp.MustCompile(`\r?\n`)
+			regxNumbers := regexp.MustCompile("[0-9]+")
+			salary = strings.Replace(salary, ",", "", -1)
+			salaries := regxNumbers.FindAllString(salary, -1)
+			var avgSalary, minSalary, maxSalary int64
+			var errMin, errMax error
+
+			fmt.Println("len ::> ", len(salaries))
+
+			if len(salaries) == 1 {
+				minSalary, errMin = strconv.ParseInt(salaries[0], 0, 64)
+				if errMin != nil {
+					fmt.Println("Error parsing min salary")
+					return
+				}
+
+				avgSalary = minSalary
+			}
+
+			if len(salaries) > 1 {
+				fmt.Println("salaries [0]: ", salaries[0])
+				fmt.Println("salaries [1] ", salaries[1])
+
+				minSalary, errMin = strconv.ParseInt(salaries[0], 0, 64)
+				if errMin != nil {
+					fmt.Println("Error parsing min salary")
+					return
+				}
+
+				maxSalary, errMax = strconv.ParseInt(salaries[1], 0, 64)
+				if errMax != nil {
+					fmt.Println("Error parsing max salary")
+					return
+				}
+
+				avgSalary = (minSalary + maxSalary) / 2
+			}
 
 			// get job details
 			jobDetails := jobDetails{
 				Type:      e.ChildText("h2.size2 > a[href]"),
 				Title:     e.ChildText("span[itemprop=title]"),
-				Salary:    re.ReplaceAllString(salary, " "),
-				Location:  re.ReplaceAllString(location, " "),
+				Salary:    avgSalary,
+				Location:  regxNewline.ReplaceAllString(location, " "),
 				Seniority: e.ChildText("span[itemprop=qualifications]"),
 				Mode:      e.ChildText("span[itemprop=employmentType]"),
 			}
 
-			//fmt.Printf("Job details %+v\n", jobDetails)
-
+			fmt.Printf("Job details %+v\n", jobDetails)
+			totalJobs++
 			switch jobDetails.Type {
 			case "Design / UX":
 				uxJobs = append(uxJobs, jobDetails)
+				totalUxJobs++
 			case "Programming":
 				programmingJobs = append(programmingJobs, jobDetails)
+				totalProgrammingJobs++
 			case "Data Science / Analytics":
 				dsJobs = append(dsJobs, jobDetails)
+				totalDsJobs++
 			case "Mobile Development":
 				mobileJobs = append(mobileJobs, jobDetails)
+				totalMobileJobs++
 			case "Customer Support":
 				supportJobs = append(supportJobs, jobDetails)
+				totalSupportJobs++
 			case "Digital Marketing":
 				marketingJobs = append(marketingJobs, jobDetails)
+				totalMarketingJobs++
 			case "SysAdmin / DevOps / QA":
 				devopsQAJobs = append(devopsQAJobs, jobDetails)
+				totalDevopsQAJobs++
 			case "Operations / Management":
 				opsManagementJobs = append(opsManagementJobs, jobDetails)
+				totalOpsManagementJobs++
 			case "Sales":
 				salesJobs = append(salesJobs, jobDetails)
+				totalSalesJobs++
 			case "Advertising & Media":
 				advertisingJobs = append(advertisingJobs, jobDetails)
+				totalAdvertisingJobs++
 			case "Innovation & Agile":
 				agileJobs = append(agileJobs, jobDetails)
+				totalAgileJobs++
 			case "People & HR":
 				hrJobs = append(hrJobs, jobDetails)
+				totalHrJobs++
 			}
 		})
 
