f2d7869ed2a78865d3f65d46edd119c0ea4ba686
krystofff <ch.camposr@gmail.com>
2021-01-11 20:41:07 -0300

    adds second approach to calculate avg and median salaries by job type!

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"regexp"
+	"sort"
 	"strconv"
 	"strings"
 
@@ -15,13 +16,22 @@ var uxJobs, programmingJobs, dsJobs, mobileJobs, supportJobs, marketingJobs, dev
 	salesJobs, advertisingJobs, agileJobs, hrJobs []jobDetails
 
 var totalJobs, totalUxJobs, totalProgrammingJobs, totalDsJobs, totalMobileJobs, totalSupportJobs, totalMarketingJobs,
-	totalDevopsQAJobs, totalOpsManagementJobs, totalSalesJobs, totalAdvertisingJobs, totalAgileJobs, totalHrJobs int
+	totalDevopsQAJobs, totalOpsManagementJobs, totalSalesJobs, totalAdvertisingJobs, totalAgileJobs, totalHrJobs int64
+
+var avgUxSalary, avgProgrammingSalary, avgMobileSalary, avgSupportSalary, avgMarketingSalary,
+	vagDevopsQASalary, avgOpsMangmentSalary, avgSalesSalary, avgAdvertisingSalary, avgAgileSalary, avgHrSalary MeanMedian
+
+var avgDsSalary []float64
+
+type MeanMedian struct {
+	numbers []float64
+}
 
 type jobDetails struct {
 	Type  string
 	Title string
 	// Role      string
-	Salary    int64
+	Salary    float64
 	Location  string
 	Seniority string
 	Mode      string
@@ -55,13 +65,19 @@ func main() {
 	/* for i := range jobTypeLinks {
 	jobCollector.Visit(jobTypeLinks[i])
 	} */
-	jobCollector.Visit(jobTypeLinks[1])
+	jobCollector.Visit(jobTypeLinks[2])
+	jobCollector.Visit(jobTypeLinks[3])
 
 	//fmt.Println("UX jobs: ", uxJobs)
 	//fmt.Println("Programming  jobs: ", programmingJobs)
 	//fmt.Println("total UX josbs: ", totalUxJobs)
+
+	fmt.Println("******************** SUMMARY ******************** ")
 	fmt.Println("total MObile josbs: ", totalMobileJobs)
+	fmt.Println("total DS josbs: ", totalDsJobs)
+	fmt.Println("------------------------------------------------ ")
 	fmt.Println("total jobs: ", totalJobs)
+	fmt.Println("avg DS salary: ", avgDsSalary)
 }
 
 // default collector get job IDs
@@ -92,13 +108,13 @@ func getJobs() {
 			regxNumbers := regexp.MustCompile("[0-9]+")
 			salary = strings.Replace(salary, ",", "", -1)
 			salaries := regxNumbers.FindAllString(salary, -1)
-			var avgSalary, minSalary, maxSalary int64
+			var avgSalary, minSalary, maxSalary float64
 			var errMin, errMax error
 
 			fmt.Println("len ::> ", len(salaries))
 
 			if len(salaries) == 1 {
-				minSalary, errMin = strconv.ParseInt(salaries[0], 0, 64)
+				minSalary, errMin = strconv.ParseFloat(salaries[0], 64)
 				if errMin != nil {
 					fmt.Println("Error parsing min salary")
 					return
@@ -111,13 +127,13 @@ func getJobs() {
 				fmt.Println("salaries [0]: ", salaries[0])
 				fmt.Println("salaries [1] ", salaries[1])
 
-				minSalary, errMin = strconv.ParseInt(salaries[0], 0, 64)
+				minSalary, errMin = strconv.ParseFloat(salaries[0], 64)
 				if errMin != nil {
 					fmt.Println("Error parsing min salary")
 					return
 				}
 
-				maxSalary, errMax = strconv.ParseInt(salaries[1], 0, 64)
+				maxSalary, errMax = strconv.ParseFloat(salaries[1], 64)
 				if errMax != nil {
 					fmt.Println("Error parsing max salary")
 					return
@@ -137,17 +153,28 @@ func getJobs() {
 			}
 
 			fmt.Printf("Job details %+v\n", jobDetails)
-			totalJobs++
+
+			// TODO: find better validation
+			if jobDetails.Type != "" {
+				totalJobs++
+			}
+
 			switch jobDetails.Type {
 			case "Design / UX":
 				uxJobs = append(uxJobs, jobDetails)
 				totalUxJobs++
+				// avgUxSalary += avgSalary
 			case "Programming":
 				programmingJobs = append(programmingJobs, jobDetails)
 				totalProgrammingJobs++
 			case "Data Science / Analytics":
 				dsJobs = append(dsJobs, jobDetails)
 				totalDsJobs++
+
+				if avgSalary > 0 {
+					avgDsSalary = append(avgDsSalary, avgSalary)
+				}
+
 			case "Mobile Development":
 				mobileJobs = append(mobileJobs, jobDetails)
 				totalMobileJobs++
@@ -181,5 +208,35 @@ func getJobs() {
 		for i := range jobDetailsLinks {
 			jobCollector.Visit(jobDetailsLinks[i])
 		}
+
+		fmt.Println("avgDsSalary ---> ", avgDsSalary)
+		fmt.Println("total ds Jobs ---> ", totalDsJobs)
+
+		// avgUxSalary = avgUxSalary / totalUxJobs
+		//avgDsSalary = avgDsSalary / totalDsJobs
+		mmType := MeanMedian{avgDsSalary}
+
+		fmt.Println("Median ::> ", mmType.calcMedian())
 	})
+}
+
+func (mm *MeanMedian) calcMedian(n ...float64) float64 {
+	sort.Float64s(mm.numbers) // sort the numbers
+
+	mNumber := len(mm.numbers) / 2
+
+	if mm.IsOdd() {
+		return mm.numbers[mNumber]
+	}
+
+	return (mm.numbers[mNumber-1] + mm.numbers[mNumber]) / 2
+}
+
+// check if the total of numbers is odd or even
+func (mm *MeanMedian) IsOdd() bool {
+	if len(mm.numbers)%2 == 0 {
+		return false
+	}
+
+	return true
 }
